d0087eec05a9a32e8c8c078fc07ba66993f73d49
Leonel Viera <vieraleonel1@gmail.com>
2019-08-01 01:05:49 -0300

    Compatibilidad con mysql del server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // para compatibilidad con MySQL < v5.7.7
+        Schema::defaultStringLength(191);
     }
 }
